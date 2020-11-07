d488d5703949780f04706ed93911eae6c6a825c3
Tomás Mateo <tomasmateo10@gmail.com>
2020-11-07 14:18:22 -0300

    i-Adding test file

---
+++ pages/api/test.js
@@ -0,0 +1,32 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig: { __SOCKETIO_SERVER } } = getConfig();
+
+export default async (req, res) => {
+
+  try {
+
+    // 4) Update in all the fronts
+    const params = {
+      method: 'POST',
+      body: JSON.stringify({
+        jwtToken: req.body.jwtToken,
+        id: subastaId,
+        amount,
+        email,
+        name,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+
+    // const { status } = await fetch(`${__SOCKETIO_SERVER}/update-sockets`, params);
+    const { status } = await fetch(`${__SOCKETIO_SERVER}/ping`);
+    const res = await res.json();
+    console.log('status: ', status);
+    console.log('response: ', response);
+    res.status(200).send({});
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+}
