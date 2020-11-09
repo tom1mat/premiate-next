826e8677df3f931d5d92034c9fc82ada6aafbc51
Tomás Mateo <tomasmateo10@gmail.com>
2020-11-08 23:58:21 -0300

    tesst

--- pages/api/sorteos/[id].js
+++ pages/api/sorteos/[id].js
@@ -86,7 +86,7 @@ const put = async (req, res) => {
         };
 
         // fetch(`${__SOCKETIO_SERVER}/update-data`, params);
-        fetch(`https://premiate.ar/socket.io/update-data`, params);
+        fetch(`https://premiate.ar/socket/update-data`, params);
       } catch (error) {
         console.error(error);
         responseStatus = 500;
